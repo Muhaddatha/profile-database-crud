5eb0657989cc1e6c5ca7d67a85c6744edf1f2fde
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2021-02-07 18:01:28 -0500

    validate email address in login page

--- scripts/script.js
+++ scripts/script.js
@@ -17,6 +17,12 @@ function validateFrom() {
 
         }
 
+        const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(re.test(email) == false){
+            alert("Invalid email address");
+            return false;
+        }
+
         return true;
 
     } catch(e) {
